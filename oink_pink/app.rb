80eb9f4a2d7e5ebd7dcc0c0a51902b62268db057
Jack Somervell <jacksomervell@gmail.com>
2015-09-02 18:00:01 +0100

    sorted the form

--- oink_pink/app.rb
+++ oink_pink/app.rb
@@ -2,6 +2,16 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require_relative './models/pig_latinize' 
 
+  get '/' do
+    @title = 'Home'
+    erb :home
+  end
+
   get '/:word' do
     "#{Latinize.pig params[:word]} "
   end
+
+  post '/translate' do
+    @pig_text = Latinize.pig params[:text]
+
+  end 
